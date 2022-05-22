f181faf8b3f44e51c72d9c84f3a87f91c03d04ed
om praksash pattjoshi <omprakashpattjoshi123@gmail.com>
2022-05-22 16:52:01 +0530

    Binding all together

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Shades from "./components/Shades";
+import Shadelnput from "./components/Shadelnput";
+import Shade from "./components/Shade";
+
+import randomcolor from "randomcolor";
 
 const App = () => {
+  const [color, setColor] = useState("#000");
+
+  useEffect(() => {
+    setColor(randomcolor);
+  }, []);
+
+  const onInputChange = (value) => {
+    setColor(value);
+  };
+
   return (
     <div>
-      <h1>Helo frim app</h1>
+      <Shadelnput color={color} onInputChange={onInputChange} />
+      <Shades color={color} />
     </div>
   );
 };
